1acea87ad7d770ca3c8dc764e76951d22ca4cf1e
Aarav Yadav <92964286+AaravyOTH@users.noreply.github.com>
2023-01-20 19:29:28 -0500

    Made the JSON appear

--- app/src/main/java/com/example/weatherproject/MainActivity.java
+++ app/src/main/java/com/example/weatherproject/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.weatherproject;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,16 +24,57 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
+    EditText editText;
+    Button ZipCodeSender;
+    String ZipCode;
+    JSONObject json;
+    String longitude;
+    String lattitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("JSON", "onCreate");
-        DownloadWeatherData asyncTask = new DownloadWeatherData();
-        asyncTask.execute("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=32d88b1ecd39ef961c7c86fe102d4406");
+
+
+        editText = findViewById(R.id.ZipCodeInput);
+        ZipCodeSender = findViewById(R.id.sendZipCode);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                ZipCode =  charSequence.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        ZipCodeSender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Log.d("zipcode", ZipCode);
+                new DownloadWeatherData().execute("https://api.openweathermap.org/data/2.5/forecast?zip="+ ZipCode +"&appid=32d88b1ecd39ef961c7c86fe102d4406");
+
+            }
+
+
+    });
     }
+    public EditText getEditText() {
+        return editText;
+    }
+
+
+
     private class DownloadWeatherData extends AsyncTask<String, Void, Void>{
 
         @Override
@@ -48,22 +95,33 @@ public class MainActivity extends AppCompatActivity {
                     builder.append(line);
                 }
                 String jsonString = builder.toString(); //String
-                JSONObject json = new JSONObject(jsonString);
+                json = new JSONObject(jsonString);
 
                 Log.d("JSON", jsonString);
 
             }catch(IOException e){
                 e.printStackTrace();
                 Log.d("JSON", "IO EXCEPTION");
-
+                Log.d("JSON", e.toString());
             } catch (JSONException e) {
                 e.printStackTrace();
             }
             return null;
         }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+
+        }
     }
+
+
 }
 
-
 //http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=32d88b1ecd39ef961c7c86fe102d4406
-//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=524901&appid=32d88b1ecd39ef961c7c86fe102d4406
+//api.openweathermap.org/data/2.5/forecast?zip=08824&appid=524901&appid=32d88b1ecd39ef961c7c86fe102d4406
+
+//https://api.openweathermap.org/data/2.5/weather?zip=[zipcode],US&appid=32d88b1ecd39ef961c7c86fe102d4406
+//THIS THE SHIT I NEED https://api.openweathermap.org/data/2.5/forecast?zip=08824&appid=32d88b1ecd39ef961c7c86fe102d4406
